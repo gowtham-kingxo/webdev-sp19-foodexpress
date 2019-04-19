0961021db1372bd6f244e9526038102955e092be
gowthamgenupur <genupur.g@husky.neu.edu>
2019-04-19 15:10:42 -0400

    Search page created

---
+++ public/services/review.service.js
@@ -0,0 +1,35 @@
+(function () {
+    angular
+        .module("FoodExpress")
+        .factory("ReviewService", reviewService);
+
+    function reviewService($http) {
+        var baseUrl = "http://localhost:5000/api/review"
+        var api = {
+            getAllReviews: getAllReviews,
+            getReview: getReview,
+            removeReview: removeReview,
+            createReview: createReview,
+            updateReview: updateReview
+        };
+        return api;
+
+        function getAllReviews() {
+            return $http.get(baseUrl);
+        }
+
+        function getReview(id) {
+            return $http.get(baseUrl + "/" + id);
+        }
+
+        function removeReview(id) {
+            return $http.delete(baseUrl + "/" + id);
+        }
+        function createReview(review){
+            return $http.post(baseUrl, review);
+        }
+        function updateReview(id,review){
+            return $http.put(baseUrl + "/" + id, review);
+        }
+    }
+})();
