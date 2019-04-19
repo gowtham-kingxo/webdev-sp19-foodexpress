c94233984f59bb6639be17b86c1ade707d488d4a
Jae Son <sjson421@gmail.com>
2019-04-19 14:07:37 -0400

    user schema

---
+++ data/models/User.schema.server.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const OwnerSchema = require("./Owner.schema.server");
+const CriticSchema = require("./Critic.schema.server");
+const RegisteredUserScehma = require("./RegisteredUser.schema.server");
+const AdvertiserSchema = require("./Advertiser.schema.server");
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 3
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 5
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 3
+    },
+    address: {
+      streetaddress: {
+        type: String,
+        required: true
+      },
+      streetaddress2: {
+        type: String
+      },
+      city: {
+        type: String,
+        required: true
+      },
+      state: {
+        type: String,
+        required: true
+      },
+      country: {
+        type: String,
+        required: true
+      },
+      zipcode: {
+        type: Number,
+        required: true
+      }
+    },
+    phone: {
+      type: Number,
+      required: true
+    },
+    picture: {
+      type: String
+    },
+    // can remove this field
+    // Add enum in userType
+    userType: {
+      type: String,
+      required: true,
+      enum: ["REGISTERED", "CRITIC", "OWNER", "ADVERTISER", "ADMIN"]
+    },
+    owner: OwnerSchema,
+    critic: CriticSchema,
+    registeredUser: RegisteredUserScehma,
+    advertiser: AdvertiserSchema,
+    date: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { collection: "users" }
+);
+
+module.exports = UserModel = mongoose.model("User", UserSchema);
