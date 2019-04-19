5b1c1d6bc23697e95f665e268e28785fa869e851
Jae Son <sjson421@gmail.com>
2019-04-19 14:01:44 -0400

    initial setup

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const user = require('./routes/user');
+const restaurant = require('./routes/restaurant');
+const featured = require('./routes/featured');
+const event = require('./routes/event');
+const advertisement = require('./routes/advertisement');
+const review = require('./routes/review');
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+const db = require('./data/db').mongoURI;
+
+mongoose.connect(db)
+        .then(conn => console.log('mongodb connected'))
+        .catch(err => console.log(err));
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        next();
+});
+// app.get('/', (req, res) => res.send('Hello world whatsup'));
+app.use('/api/user', user);
+app.use('/api/restaurant', restaurant);
+app.use('/api/event', event);
+app.use('/api/review',review);
+app.use('/api/advertisement', advertisement);
+app.use('/api/featured', featured);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
