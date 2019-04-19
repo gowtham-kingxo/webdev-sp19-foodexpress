3e73472cc64f1c708e76c3be38e99903aba49585
gowthamgenupur <genupur.g@husky.neu.edu>
2019-04-19 15:07:52 -0400

    Feature - featured restaurants completed

---
+++ public/services/advertisement.service.js
@@ -0,0 +1,39 @@
+(function () {
+    angular
+        .module("FoodExpress")
+        .factory("AdvertisementService", advertisementService);
+
+    function advertisementService($http) {
+        var baseUrl = "http://localhost:5000/api/advertisement"
+        var api = {
+           getAdvertisements: getAdvertisements,
+           getAdvertisement: getAdvertisement,
+           createAdvertisement: createAdvertisement,
+           updateAdvertisement: updateAdvertisement,
+           deleteAdvertisement: deleteAdvertisement
+        };
+        return api;
+      
+        function getAdvertisements(){
+            return $http.get(baseUrl);
+        }
+
+        function getAdvertisement(id){
+            return $http.get(baseUrl + "/" + id);
+        }
+
+        function createAdvertisement(advertisement){
+            return $http.post(baseUrl, advertisement);
+        }
+
+        function updateAdvertisement(id, advertisement){
+            console.log("advertisement is",advertisement);
+            return $http.put(baseUrl + "/" + id, advertisement);
+        }
+
+        function deleteAdvertisement(id){
+            return $http.delete(baseUrl + "/" + id);
+        }
+
+    }
+})();
