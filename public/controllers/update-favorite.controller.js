0fc2bef2a4a078fdba4a1544a9cb9a6a7fcfe3ac
gowthamgenupur <genupur.g@husky.neu.edu>
2019-04-19 15:17:38 -0400

    Working on restaurant details page

---
+++ public/controllers/update-favorite.controller.js
@@ -0,0 +1,34 @@
+(function () {
+    angular
+        .module("FoodExpress")
+        .controller("UpdateFavoriteController", function ($rootScope, $scope, $window, $routeParams, RestaurantService, UserService) {
+            const userId = $routeParams.userId;
+            const temp = $routeParams.restaurantId;
+
+            UserService.getUser(userId)
+                .then(function (response) {
+                    $scope.username = response.data.user.username;
+                });
+            RestaurantService.getAllRestaurants()
+                .then(function(response) {
+                    $scope.allRestaurants = response.data;
+                });
+
+            $scope.updateFavorite = function () {
+                const string = $scope.restaurant;
+                const res = string.split(" ");
+                const restaurantId = res[1];
+                console.log(temp);
+                console.log(restaurantId);
+                UserService.updateFavorite(userId, restaurantId, temp)
+                    .then(
+                        function (response) {
+                            alert("Favorite successfully updated!")
+                        },
+                        function (err) {
+                            console.log(err);
+                        }
+                    )
+            };
+        });
+})();
