41d2f922007acf822afb62e976f0a92e27ccd283
Jae Son <sjson421@gmail.com>
2019-04-20 04:45:48 -0400

    pathing progress

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const advertisement = require('./routes/advertisement');
 const review = require('./routes/review');
 const app = express();
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -20,29 +22,48 @@ app.use(morgan('tiny'));
 const db = require('./data/db').mongoURI;
 
 mongoose.connect(db)
-        .then(conn => console.log('mongodb connected'))
-        .catch(err => console.log(err));
+    .then(conn => console.log('mongodb connected'))
+    .catch(err => console.log(err));
 
 
 app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
 });
 
 app.use('/api/user', user);
 app.use('/api/restaurant', restaurant);
 app.use('/api/event', event);
-app.use('/api/review',review);
+app.use('/api/review', review);
 app.use('/api/advertisement', advertisement);
 app.use('/api/featured', featured);
 
-app.get('/*', function(req, res) {
-    res.sendFile('public/index.html' , { root : __dirname});
+app.use('*controllers*', function (req, res, next) {
+
+})
+app.use('*css*', function (req, res, next) {
+    next();
+})
+app.use('*fonts*', function (req, res, next) {
+    next();
+})
+app.use('*images*', function (req, res, next) {
+    next();
+})
+app.use('*scss*', function (req, res, next) {
+    next();
+})
+app.use('*services*', function (req, res, next) {
+    next();
+})
+app.use('*views*', function (req, res, next) {
+    next();
+})
+
+app.use('/*', function (req, res) {
+    console.log("entered")
+    res.sendFile('public/index.html', {root: __dirname});
 });
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on ${port}`));
